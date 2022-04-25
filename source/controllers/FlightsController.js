b2aa61af8d39dba7dd2fdb92da587238a9b5feab
Riuregan <henrique.henri25@gmail.com>
2022-04-25 11:35:08 -0300

    part 2 working

--- source/controllers/FlightsController.js
+++ source/controllers/FlightsController.js
@@ -1,25 +1,21 @@
 import { getAllFlights } from "../models/FlightsModel.js"
 
-export async function getAll(req, res) {
-    const allFlights = await getAllFlights();
+export async function getAll(req, res, flightsCollection) {
+    const allFlights = await flightsCollection.find({}).toArray();
+    console.log(allFlights);
     res.writeHead(200, { 'Content-Type': 'application/json' });
 
     res.end(JSON.stringify(allFlights));
 }
-export async function getById(req, res, num) {
-    const allFlights = await getAllFlights();
-    for (var index in allFlights) {
-        if (allFlights.hasOwnProperty(index)) {
-            if (allFlights[index]["id"] == num) {
-                var answerById = allFlights[index];
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(JSON.stringify(answerById));
-            }
+export async function getById(req, res, num, flightsCollection) {
+    const query = { id: num }
+    const answerById = await flightsCollection.find(query).toArray();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(answerById));
 
-        }
-    }
 }
 export async function post(req, res, id, flightsCollection) {
+    //console.log(flightsCollection);
     const allFlights = await getAllFlights();
 
     let body = [];
@@ -31,46 +27,45 @@ export async function post(req, res, id, flightsCollection) {
         res.on('error', (err) => {
             console.error(err);
         });
-
-        //await flightsCollection.insertOne(bodyObject);
+        console.log(bodyObject);
+        await flightsCollection.insertOne(bodyObject);
 
         allFlights[id - 1] = bodyObject;
 
         res.writeHead(200, { "Content-Type": "application/json" });
-        //res.end(JSON.stringify(allFlights));
         res.write(JSON.stringify(allFlights));
         res.end();
     });
 }
 
-export async function put(req, res, id, flightsCollection) {
-    const allFlights = await getAllFlights();
+export async function put(req, res, num, flightsCollection) {
+    const query = { id: num }
+    //const allFlights = await getAllFlights();
     let body = [];
     req.on('data', (chunk) => {
         body.push(chunk);
-    }).on('end', () => {
+    }).on('end', async () => {
         //arrumar o put
         body = Buffer.concat(body).toString();
         const objBody = JSON.parse(body);
         for (const [key, value] of Object.entries(objBody)) {
-            allFlights[id - 1][key] = value;
+            flightsCollection.updateOne(query, {
+                $set: {
+                    [key]: value
+                }
+            })
+            //allFlights[id - 1][key] = value;
         }
-
+        const allFlights = await flightsCollection.find({}).toArray();
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(allFlights));
     });
 }
-export async function deletes(req, res, num) {
-    const allFlights = await getAllFlights();
-    for (var index in allFlights) {
-        if (allFlights.hasOwnProperty(index)) {
-            if (allFlights[index]["id"] === num) {
-                allFlights.splice(index, 1);
-                res.writeHead(200, { "Content-Type": "application/json" });
+export async function deletes(req, res, num, flightsCollection) {
+    const query = { id: num }
+    flightsCollection.deleteOne(query);
+    const allFlights = await flightsCollection.find({}).toArray();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
-                res.end(JSON.stringify(allFlights));
-            }
-
-        }
-    }
+    res.end(JSON.stringify(allFlights));
 }
--- source/server.js
+++ source/server.js
@@ -2,38 +2,43 @@ import { createServer } from 'http';
 import { getAll, getById, post, put, deletes } from "./controllers/FlightsController.js";
 import { MongoClient } from 'mongodb';
 
+var flightsCollection;
+
 const uri = "mongodb+srv://riuregan:torrada186@cluster0.se4ge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-MongoClient.connect(uri, (err, client) => {
+MongoClient.connect(uri, handleConnect);
+
+function handleConnect(err, client) {
     // ... do something here
-    if (err) return console.error(err)
+    if (err) return console.error(err);
     console.log('Connected to Database');
     const db = client.db("flights-database");
-    var flightsCollection = db.collection("flights");
-})
+    flightsCollection = db.collection("flights");
 
+}
 
 const server = createServer((req, res) => {
     var path = req.url.split("/");
-    //const x = flightsCollection;
     const route = path[1];
     const id = path[2];
     if (route === "flights") {
         switch (req.method) {
             case "GET":
                 if (id === undefined) {
-                    getAll(req, res)
+                    getAll(req, res, flightsCollection);
                 } else {
-                    getById(req, res, id);
+                    getById(req, res, id, flightsCollection);
                 }
                 break;
             case "POST":
-                post(req, res, id, x);
+                post(req, res, id, flightsCollection);
                 break;
             case "PUT":
-                put(req, res, id);
+                console.log("put ");
+                put(req, res, id, flightsCollection);
                 break;
             case "DELETE":
-                deletes(req, res, id);
+                console.log("delete ");
+                deletes(req, res, id, flightsCollection);
                 break;
             default:
                 res.end(JSON.stringify({ mensagem: "unsupported operation" }));
